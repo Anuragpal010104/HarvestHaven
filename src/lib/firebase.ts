f561697e3961bbd4b39beaa9151dc0a73686685e
Anurag Pal <anuragpal2004@gmail.com>
2025-03-13 01:04:13 +0530

    added env.local

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBfnOBgcFYkrE8uczMMX0ITsazixhL1aYU",
-  authDomain: "harvestheaven-7feba.firebaseapp.com",
-  projectId: "harvestheaven-7feba",
-  storageBucket: "harvestheaven-7feba.firebasestorage.app",
-  messagingSenderId: "123085761189",
-  appId: "1:123085761189:web:f11fa5a699e281124bfb4c",
-  measurementId: "G-WXCSTDFJ91"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
